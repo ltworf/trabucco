796f52f92a69c14feafb98b40b20d8385c62e942
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-02-13 22:34:24 +0100

    Icon searching utility
    It is not really close to what freedesktop.org defines but
    who cares. The problem is that freedesktop.org talks about themes
    but does not tell where can the theme setting be found.

---
+++ iconfinder.cpp
@@ -0,0 +1,58 @@
+#include "iconfinder.h"
+
+#include <QFileInfo>
+
+
+
+QString IconFinder::FindIcon(QString icon) {
+    static const char * dirs[]= {
+        "/usr/share/icons/breeze-dark/",
+        "/usr/share/icons/breeze/",
+        "/usr/share/icons/oxygen/",
+        "/usr/share/icons/hicolor/",
+    };
+
+    static const char * paths[]= {
+        "scalable/apps/",
+        "512x512/apps/",
+        "apps/512/",
+        "256x256/apps/",
+        "apps/256/",
+        "192x192/apps/",
+        "128x128/apps/",
+        "apps/128/",
+        "64x64/apps/",
+        "apps/64/",
+        "apps/48/",
+        "32x32/apps/",
+        "apps/32/",
+        "apps/22/",
+        "16x16/apps/",
+        "apps/16/",
+    };
+
+    static const char * formats[]= {
+        ".png",
+        ".svg",
+        ".svgz",
+    };
+
+    for (unsigned int d=0; d< sizeof(dirs)/sizeof(int*); d++) {
+        for (unsigned int p=0; p<sizeof(paths)/sizeof(int*); p++) {
+            for (unsigned int f=0; f<sizeof(formats)/sizeof(int*); f++) {
+                QString attempt = QString(dirs[d]) + QString(paths[p]) + icon + QString(formats[f]);
+                printf("Attempting %s\n", attempt.toStdString().c_str());
+                QFileInfo i(attempt);
+                if (i.exists() && i.isReadable()) {
+                    printf("Succeeded\n");
+                    return attempt;
+                }
+            }
+        }
+    }
+
+
+
+    return "";
+}
+
---
+++ iconfinder.h
@@ -0,0 +1,12 @@
+#ifndef ICONFINDER_H
+#define ICONFINDER_H
+
+#include <QString>
+
+class IconFinder
+{
+public:
+    static QString FindIcon(QString icon);
+};
+
+#endif // ICONFINDER_H
