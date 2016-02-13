51c1bf31a40b7e2faada2b08a996e2d8f6cf9c72
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-02-13 20:06:12 +0100

    Export objects to QML

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <qqmlcontext.h>
+#include <QtQml>
+
+#include "tree.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    Tree t;
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("tree", &t);
+    qmlRegisterType<Action>("random.namespace", 1, 0, "Action");
+
+
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
