a3e2674c517d592eecbbf3eaea2364799ec6ce9c
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-02-13 16:15:19 +0100

    DesktopActions can be loaded from the FS
    Implemented static loader

--- desktopaction.cpp
+++ desktopaction.cpp
@@ -1,4 +1,6 @@
 #include <QSettings>
+#include <QDirIterator>
+#include <QFileInfo>
 
 #include "desktopaction.h"
 
@@ -7,9 +9,36 @@ DesktopAction::DesktopAction(QString file, QObject *parent): Action(parent) {
     this->name = settings.value("Desktop Entry/Name","").toString();
     this->icon = settings.value("Desktop Entry/Icon","").toString();
     this->action = settings.value("Desktop Entry/Exec","").toString();
-    this->terminal = settings.value("Desktop Entry/Terminal","").toBool();
+    this->terminal = settings.value("Desktop Entry/Terminal","false").toBool();
 }
 
 void DesktopAction::runAction() {
     printf("%s\n", this->action.toStdString().c_str());
+}
+
+static void iterate_dir(QStack<DesktopAction*> * result, QString dir) {
+
+    // Include subdirectories and follow links
+    QDirIterator i(dir, QDirIterator::Subdirectories | QDirIterator::FollowSymlinks);
+
+    while (i.hasNext()) {
+        QString path = i.next();
+        QFileInfo info(path);
+
+        if (info.isFile() && info.isReadable()) {
+            DesktopAction * action = new DesktopAction(path);
+            result->push(action);
+        }
+
+    }
+}
+
+QStack<DesktopAction*> DesktopAction::LoadDesktopActions() {
+    QStack<DesktopAction*> result;
+
+    iterate_dir(&result,"/usr/share/applications/");
+    //TODO add local directories
+
+
+    return result;
 }
--- desktopaction.h
+++ desktopaction.h
@@ -2,6 +2,7 @@
 #define DESKTOPACTION_H
 
 #include <QString>
+#include <QStack>
 
 #include "action.h"
 
@@ -9,10 +10,13 @@ class DesktopAction: public Action
 {
 public:
     DesktopAction(QString file,QObject * parent=0);
+    static QStack<DesktopAction*> LoadDesktopActions();
 public slots:
     virtual void runAction();
 private:
     bool terminal;
 };
+
+
 
 #endif // DESKTOPACTION_H
