45601eb0901d5f7bd9cac073374539d7d495f641
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-02-14 22:53:30 +0100

    Create key grabber

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@
 #include <QtQml>
 
 #include "tree.h"
+#include "shortcutactivator.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     Tree t;
+
+    ShortcutActivator shortcut;
+
+    shortcut.start();
+
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("tree", &t);
+    engine.rootContext()->setContextProperty("ShortcutX11", &shortcut);
     qmlRegisterType<Action>("random.namespace", 1, 0, "Action");
 
 
