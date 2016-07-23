982c02baaf2485ac7de19090e476e0487824c776
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-07-23 20:29:09 +0200

    Added copyright notice to every source file

--- btreeiterator.h
+++ btreeiterator.h
@@ -1,3 +1,22 @@
+/*
+This file is part of Trabucco.
+
+Trabucco is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Trabucco is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Trabucco.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2016  Salvo "LtWorf" Tomaselli
+*/
+
 #ifndef BTREEITERATOR_H
 #define BTREEITERATOR_H
 
