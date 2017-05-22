7175686e257943be2f44f351038f0a21472143be
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2017-05-22 19:56:37 +0200

    Rewrite self-reloading code
    The previous one was not working because too
    many resources remained open after closing the QApplication.
    
    This closes the entire process so all the resources are freed.
    
    An additional complication comes from the fact that trabucco only allows
    one instance per session, so a --wait parameter is added, and if that is
    present, some more time is given to aquire the lock.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ along with Trabucco.  If not, see <http://www.gnu.org/licenses/>.
 Copyright (C) 2016  Salvo "LtWorf" Tomaselli
 Copyright (C) 2016 Giuseppe Bilotta
 */
+#include <unistd.h>
 
 #include <QApplication>
 #include <QQmlApplicationEngine>
@@ -32,21 +33,34 @@ Copyright (C) 2016 Giuseppe Bilotta
 #include "settings.h"
 #include "singleinstance.h"
 
-int real_main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     app.setApplicationName("Trabucco");
     app.setOrganizationDomain("SiegeSoftware");
+    bool wait = argc == 2 && QString(argv[1]) == "--wait";
 
     QString icon_path = IconFinder::FindIcon("trabucco");
     QSystemTrayIcon tray;
     tray.setIcon(QIcon(icon_path));
     tray.show();
 
+    //Ensure unique instance, but allow for previous self to die
     if (!SingleInstance::unique("trabucco", SESSION)) {
+        int retry = wait ? 30 : 0;
+
+        while (retry > 0) {
+            qDebug() << "wait..";
+            if (SingleInstance::unique("trabucco", SESSION)) {
+                goto is_unique;
+            }
+            retry--;
+            sleep(1);
+        }
         qDebug() << "Already running";
         tray.showMessage("Trabucco","Trabucco is already running.", QSystemTrayIcon::Warning, 4000);
         return 1;
     }
+    is_unique:
 
     Cache::create_cache_dir();
     Tree t;
@@ -80,14 +94,11 @@ int real_main(int argc, char *argv[]) {
 
     shortcut.start();
     tray.showMessage("Trabucco","Trabucco is running. Press " + shortcut.shortcut_name(), QSystemTrayIcon::Information, 2000);
-    return app.exec();
-}
-
-int main(int argc, char *argv[]) {
-    // A trick to reload the configuration
-    while(true) {
-        int r = real_main(argc, argv);
-        if (r != CONFIG_RELOAD)
-            return r;
+    int r = app.exec();
+    if (r == CONFIG_RELOAD) {
+        QStringList args;
+        args.append("--wait");
+        QProcess::startDetached(QApplication::applicationFilePath(), args);
     }
+    return r;
 }
