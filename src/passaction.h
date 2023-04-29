2378f7a26fe6bb828b6be7dc12e3b7cfb6c2e136
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2023-04-30 00:23:49 +0200

    Scan the pass files

---
+++ src/passaction.h
@@ -0,0 +1,45 @@
+/*
+This file is part of Trabucco.
+
+Trabucco is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Trabucco is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Trabucco.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2023  Salvo "LtWorf" Tomaselli
+*/
+
+#ifndef PASSACTION_H
+#define PASSACTION_H
+
+#include <QString>
+#include <QStringList>
+
+#include "action.h"
+#include "btree.h"
+
+class PassAction: public Action {
+public:
+    PassAction(QString script, QString prefix, QObject * parent=NULL);
+    static void LoadPassActions(BTree*, QObject* parent=NULL);
+    static QStringList* GetPaths();
+    static bool isPassInstalled();
+public slots:
+    virtual void runAction();
+    virtual QString getIcon();
+//    bool mustShow();
+private:
+    QString script;
+    QString cached_icon_path;
+    static void scanAndLoad(BTree* tree, QObject* parent, QString base, QString icon, QStringList prefixes);
+};
+
+#endif // PASSACTION_H
